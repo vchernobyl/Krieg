5fa2e2f8e7e4300a05c34df24db4d00fdebc75a0
vchernobyl <vitalijskrumins@gmail.com>
2021-03-08 20:44:42 +0100

    Basic camera movement from within the background component

--- src/BackgroundComponent.cpp
+++ src/BackgroundComponent.cpp
@@ -1,32 +1,41 @@
 #include "BackgroundComponent.h"
+#include "InputSystem.h"
 #include "Actor.h"
 
-static const int WorldWidth = 1280;
-static const int WorldHeight = 960;
+static const int WorldWidth = 1728;
+static const int WorldHeight = 1080;
 static const int CameraWidth = 1024;
 static const int CameraHeight = 768;
 
-BackgroundComponent::BackgroundComponent(Actor* owner, int updateOrder) :
-    SpriteComponent(owner, updateOrder) {
+BackgroundComponent::BackgroundComponent(Actor* owner, int updateOrder)
+    : SpriteComponent(owner, updateOrder),
+      camera({ 0, 0, CameraWidth, CameraHeight }),
+      background({ 0, 0, WorldWidth, WorldHeight }) {
     SetTexture(owner->GetGame()->GetTexture("assets/background.png"));
 }
 
+void BackgroundComponent::ProcessInput(const InputState& input) {
+    if (input.Keyboard.GetKeyValue(SDL_SCANCODE_D)) {
+	camera.x += 50;
+    }
+    if (input.Keyboard.GetKeyValue(SDL_SCANCODE_A)) {
+	camera.x -= 50;
+    }
+    if (input.Keyboard.GetKeyValue(SDL_SCANCODE_S)) {
+	camera.y += 50;
+    }
+    if (input.Keyboard.GetKeyValue(SDL_SCANCODE_W)) {
+	camera.y -= 50;
+    }
+    if (camera.x < 0) camera.x = 0;
+    if (camera.x > WorldWidth - CameraWidth) camera.x = WorldWidth - CameraWidth;
+    if (camera.y < 0) camera.y = 0;
+    if (camera.y > WorldHeight - CameraHeight) camera.y = WorldHeight - CameraHeight;
+}    
+
 void BackgroundComponent::Draw(SDL_Renderer* renderer) {
     if (texture) {
-	Vector2 pos = owner->GetPosition();
 	SDL_Rect background { 0, 0, WorldWidth, WorldHeight };
-	SDL_Rect camera {
-	    static_cast<int>(owner->GetPosition().x - CameraWidth / 2),
-	    static_cast<int>(owner->GetPosition().y - CameraHeight / 2),
-	    CameraWidth,
-	    CameraHeight
-	};
-	SDL_Log("camera.x = %d, camera.y = %d", camera.x, camera.y);	
-	if (camera.x < 0) camera.x = 0;
-	if (camera.y < 0) camera.y = 0;
-	if (camera.x > WorldWidth - CameraWidth) camera.x = WorldWidth - CameraWidth;
-	if (camera.y > WorldHeight - CameraHeight) camera.y = WorldHeight - CameraHeight;
-
 	SDL_RenderCopy(renderer, texture, &camera, &background);
     }
 }
--- src/BackgroundComponent.h
+++ src/BackgroundComponent.h
@@ -5,5 +5,9 @@
 class BackgroundComponent : public SpriteComponent {
 public:
     BackgroundComponent(Actor* owner, int updateOrder = 10);
+    void ProcessInput(const InputState& input) override;
     void Draw(SDL_Renderer* renderer) override;
+private:
+    SDL_Rect camera;
+    SDL_Rect background;
 };
