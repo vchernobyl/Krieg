a8c5a81006400ba8ef983b26117e742a71c9161f
vchernobyl <vitalijskrumins@gmail.com>
2021-04-15 21:22:14 +0200

    Generate all ouput files inside the build folder

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-cl /I "C:\libs\SDL2-2.0.14\include" /I "C:\libs\SDL2_image-2.0.5\include" src\*.cpp shell32.lib /link /LIBPATH "C:\libs\SDL2-2.0.14\lib\x64\SDL2.lib" "C:\libs\SDL2-2.0.14\lib\x64\SDL2main.lib" "C:\libs\SDL2_image-2.0.5\lib\x64\SDL2_image.lib" /SUBSYSTEM:CONSOLE
+cd build\
+cl /Fe"Game.exe" /Zi /I "C:\libs\SDL2-2.0.14\include" /I "C:\libs\SDL2_image-2.0.5\include" ..\src\*.cpp shell32.lib /link /LIBPATH "C:\libs\SDL2-2.0.14\lib\x64\SDL2.lib" "C:\libs\SDL2-2.0.14\lib\x64\SDL2main.lib" "C:\libs\SDL2_image-2.0.5\lib\x64\SDL2_image.lib" /SUBSYSTEM:CONSOLE
+cd ..
