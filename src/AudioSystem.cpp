ecfa4702a1026a4b7f8ab99851ebe1e5782b56d3
vchernobyl <vitalijskrumins@gmail.com>
2021-05-03 22:51:20 +0200

    Implement FMOD Studio API initialization

--- src/AudioSystem.cpp
+++ src/AudioSystem.cpp
@@ -1,14 +1,33 @@
 #include "AudioSystem.h"
 #include "Game.h"
-#include <fmod_studio.h>
 
-AudioSystem::AudioSystem(Game* game) : game(game) {}
+#include <fmod_studio.hpp>
+#include <fmod_errors.h>
 
-AudioSystem::~AudioSystem() {}
+AudioSystem::AudioSystem(Game* game) : game(game), system(nullptr) {}
 
 bool AudioSystem::Initialize() {
-    return false;
+    FMOD::Debug_Initialize(FMOD_DEBUG_LEVEL_ERROR, FMOD_DEBUG_MODE_TTY);
+
+    FMOD_RESULT result;
+    result = FMOD::Studio::System::create(&system);
+    if (result != FMOD_OK) {
+	SDL_Log("Failed to create FMOD system: %s", FMOD_ErrorString(result));
+	return false;
+    }
+
+    result = system->initialize(512, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, nullptr);
+    if (result != FMOD_OK) {
+	SDL_Log("Failed to initialize FMOD system: %s", FMOD_ErrorString(result));
+	return false;
+    }
+    
+    return true;
 }
 
-void AudioSystem::Shutdown() {}
+void AudioSystem::Shutdown() {
+    if (system) {
+	system->release();
+    }
+}
     
--- src/AudioSystem.h
+++ src/AudioSystem.h
@@ -1,13 +1,25 @@
+#pragma once
+
+namespace FMOD {
+    class System;
+    namespace Studio {
+	class Bank;
+	class EventDescription;
+	class EventInstance;
+	class System;
+	class Bus;
+    }
+};
+
 class Game;
 
 class AudioSystem {
 public:
     AudioSystem(Game* game);
-    ~AudioSystem();
-
     bool Initialize();
     void Shutdown();
     void Update(float deltaTime);
 private:
     Game* game;
+    FMOD::Studio::System* system;
 };
