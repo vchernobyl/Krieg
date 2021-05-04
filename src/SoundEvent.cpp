a3948e12d6c67247f5190b52cfb85373964a3303
vchernobyl <vitalijskrumins@gmail.com>
2021-05-04 22:59:17 +0200

    Add empty SoundEvent method implementations

--- src/SoundEvent.cpp
+++ src/SoundEvent.cpp
@@ -1,5 +1,41 @@
 #include "SoundEvent.h"
 #include "AudioSystem.h"
 
-SoundEvent::SoundEvent() {}
-SoundEvent::SoundEvent(AudioSystem* system, unsigned int id) {}
+#include <fmod_studio.hpp>
+
+SoundEvent::SoundEvent()
+    : system(nullptr),
+      ID(0) {}
+
+SoundEvent::SoundEvent(AudioSystem* system, unsigned int id)
+    : system(system),
+      ID(id) {}
+
+bool SoundEvent::IsValid() {
+    return system && system->GetEventInstance(ID) != nullptr;
+}
+
+void SoundEvent::Restart() {
+    const auto event = system ? system->GetEventInstance(ID) : nullptr;
+    if (event) {
+	event->start();
+    }
+}
+
+void SoundEvent::Stop(bool allowFadeOut) {}
+
+void SoundEvent::SetPaused(bool pause) {}
+
+void SoundEvent::SetVolume(float value) {}
+
+void SoundEvent::SetPitch(float value) {}
+
+void SoundEvent::SetParameter(const std::string& name, float value) {}
+
+bool SoundEvent::GetPaused() const { return false; }
+
+float SoundEvent::GetVolume() const { return 0; }
+
+float SoundEvent::GetPitch() const { return 0; }
+
+float SoundEvent::GetParameter(const std::string& name) { return 0; }
