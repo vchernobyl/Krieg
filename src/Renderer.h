a74b14fb325b80577ef415d4582e25380552d457
Vitaly <vitalijskrumins@gmail.com>
2021-05-09 21:57:02 +0200

    Add top level forward declarations in the renderer

--- src/Renderer.h
+++ src/Renderer.h
@@ -3,9 +3,13 @@
 #include "SDL.h"
 #include <vector>
 
+class Game;
+class Camera;
+class SpriteComponent;
+
 class Renderer {
 public:
-    Renderer(class Game*);
+    Renderer(Game*);
     bool Initialize(int screenWidth, int screenHeight);
     void Shutdown();
     void Begin();
@@ -13,14 +17,14 @@ public:
     void DrawTexture(SDL_Texture* texture, SDL_Rect* dst, SDL_RendererFlip flip = SDL_FLIP_NONE);
     void DrawTexture(SDL_Texture* texture, SDL_Rect* src, SDL_Rect* dst, SDL_RendererFlip flip = SDL_FLIP_NONE);
     void End();
-    void AddSprite(class SpriteComponent* sprite);
-    void RemoveSprite(class SpriteComponent* sprite);
+    void AddSprite(SpriteComponent* sprite);
+    void RemoveSprite(SpriteComponent* sprite);
     SDL_Renderer* GetSDLRenderer() const { return renderer; }
-    class Camera* GetCamera() const { return camera; }
+    Camera* GetCamera() const { return camera; }
 private:
     SDL_Window* window;
     SDL_Renderer* renderer;
-    std::vector<class SpriteComponent*> sprites;
-    class Game* game;
-    class Camera* camera;
+    std::vector<SpriteComponent*> sprites;
+    Game* game;
+    Camera* camera;
 };
