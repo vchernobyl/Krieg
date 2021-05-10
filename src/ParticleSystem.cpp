8157d93ac99e4e83b64c36b66f52df1f2305be03
Vitaly <vitalijskrumins@gmail.com>
2021-05-10 09:42:15 +0200

    Added empty particle system class

--- src/ParticleSystem.cpp
+++ src/ParticleSystem.cpp
@@ -4,17 +4,9 @@
 
 #include <SDL.h>
 
-Particle::Particle(Vector2 position) {
-    this->position.x = position.x - 5 + (Random::GetFloatRange(0.0f, 25.0f));
-    this->position.y = position.y - 5 + (Random::GetFloatRange(0.0f, 25.0f));
-    lifetime = Random::GetIntRange(0, 5);
-}
+Particle::Particle(Game* game)
+    : Actor(game), lifetime(10)  {}
 
-void Particle::Draw(Renderer* renderer) {
-    SDL_Rect dst = { static_cast<int>(position.x), static_cast<int>(position.y), 4, 4 };
-    renderer->DrawTexture(texture, &dst);
-}
-
-void Particle::Update(float deltaTime) {
+void Particle::UpdateActor(float deltaTime) {
     lifetime++;
 }
--- src/ParticleSystem.h
+++ src/ParticleSystem.h
@@ -1,20 +1,29 @@
 #pragma once
 
 #include "Math.h"
+#include "Actor.h"
 
+#include <vector>
+
+class Game;
 class Renderer;
 struct SDL_Texture;
 
-const int TotalParticles = 20;
-
-class Particle {
+class Particle : public Actor {
 public:
-    Particle(Vector2 position);
-    void Draw(Renderer* renderer);
-    void Update(float deltaTime);
+    Particle(Game* game);
+    void UpdateActor(float deltaTime) override;
     bool IsDead() const { return lifetime <= 0; }
 private:
     Vector2 position;
     int lifetime;
     SDL_Texture* texture;
+};
+
+class ParticleSystem : public Actor {
+public:
+    ParticleSystem(Game* game, int maxParticles = 100);
+    void UpdateActor(float deltaTime) override;
+private:
+    std::vector<Particle*> particles;
 };
