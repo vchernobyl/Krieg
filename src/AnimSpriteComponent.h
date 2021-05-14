f04b9db1f1bad7a91de1be2b60ebf134205707a1
Vitaly <vitalijskrumins@gmail.com>
2021-05-14 09:21:40 +0200

    Remove SDL texture from the animation sprite component

--- src/AnimSpriteComponent.h
+++ src/AnimSpriteComponent.h
@@ -3,27 +3,27 @@
 #include "SpriteComponent.h"
 #include <vector>
 
-// struct AnimFrameData {
-//     int startFrame;
-//     int numFrames;
-//     int isLooping;
-// };
+struct AnimFrameData {
+    int startFrame;
+    int numFrames;
+    int isLooping;
+};
 
-// struct AnimData {
-//     std::vector<SDL_Texture*> images;
-//     std::vector<AnimFrameData> frameInfo;
-// };
+struct AnimData {
+    std::vector<class Texture*> images;
+    std::vector<AnimFrameData> frameInfo;
+};
 
-// class AnimSpriteComponent : public SpriteComponent {
-// public:
-//     AnimSpriteComponent(class Actor* owner, int drawOrder = 100);
-//     void SetAnimData(const AnimData& animData) { this->animData = animData; }
-//     void SetAnimFPS(float animFPS) { this->animFPS = animFPS; }
-//     void Update(float deltaTime) override;
-//     void ChangeAnim(int animNum);
-//     bool Finished(int animNum);
-// private:
-//     AnimData animData;
-//     int animNum, frameNum;
-//     float frameTime, animFPS;
-// };
+class AnimSpriteComponent : public SpriteComponent {
+public:
+    AnimSpriteComponent(class Actor* owner, int drawOrder = 100);
+    void SetAnimData(const AnimData& animData) { this->animData = animData; }
+    void SetAnimFPS(float animFPS) { this->animFPS = animFPS; }
+    void Update(float deltaTime) override;
+    void ChangeAnim(int animNum);
+    bool Finished(int animNum);
+private:
+    AnimData animData;
+    int animNum, frameNum;
+    float frameTime, animFPS;
+};
