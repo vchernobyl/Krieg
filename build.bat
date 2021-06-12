fe95000f7ed370aba500fc5039367ec80b1542fe
Vitaly <vitalijskrumins@gmail.com>
2021-06-12 14:10:01 +0200

    Add SDL2 to the /external

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 if not exist build mkdir build
 
 pushd build
-cl /nologo /Fe:Game.exe /DDEBUG /EHsc /MP /Zi /I ..\external\pugixml\include /I C:\libs\SDL2-2.0.14\include /I ..\external\SDL2_image\include /I "C:\Program Files (x86)\FMOD SoundSystem\FMOD Studio API Windows\api\studio\inc" /I "C:\Program Files (x86)\FMOD SoundSystem\FMOD Studio API Windows\api\core\inc" ..\src\*.cpp ..\external\pugixml\lib\pugixml.lib "C:\Program Files (x86)\FMOD SoundSystem\FMOD Studio API Windows\api\core\lib\x64\fmod_vc.lib" shell32.lib C:\libs\SDL2-2.0.14\lib\x64\SDL2.lib C:\libs\SDL2-2.0.14\lib\x64\SDL2main.lib ..\external\SDL2_image\lib\x64\SDL2_image.lib "C:\Program Files (x86)\FMOD SoundSystem\FMOD Studio API Windows\api\studio\lib\x64\fmodstudio_vc.lib" /link /DEBUG:FASTLINK /SUBSYSTEM:CONSOLE
+cl /nologo /Fe:Game.exe /DDEBUG /EHsc /MP /Zi /I ..\external\pugixml\include /I ..\external\SDL2\include /I ..\external\SDL2_image\include /I "C:\Program Files (x86)\FMOD SoundSystem\FMOD Studio API Windows\api\studio\inc" /I "C:\Program Files (x86)\FMOD SoundSystem\FMOD Studio API Windows\api\core\inc" ..\src\*.cpp ..\external\pugixml\lib\pugixml.lib "C:\Program Files (x86)\FMOD SoundSystem\FMOD Studio API Windows\api\core\lib\x64\fmod_vc.lib" shell32.lib ..\external\SDL2\lib\x64\SDL2.lib ..\external\SDL2\lib\x64\SDL2main.lib ..\external\SDL2_image\lib\x64\SDL2_image.lib "C:\Program Files (x86)\FMOD SoundSystem\FMOD Studio API Windows\api\studio\lib\x64\fmodstudio_vc.lib" /link /DEBUG:FASTLINK /SUBSYSTEM:CONSOLE
 popd
 
 echo.
