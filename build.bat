a38ffc5ad38a56dbb43f832e1b9f109020a8977e
Vitaly <vitalijskrumins@gmail.com>
2021-06-13 18:50:52 +0200

    Add box2d to the /external

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 if not exist build mkdir build
 
 pushd build
-cl /nologo /Fe:Game.exe /DDEBUG /EHsc /MP /Zi /I ..\external\pugixml\include /I ..\external\SDL2\include /I ..\external\SDL2_image\include /I ..\external\FMOD\api\studio\include /I ..\external\FMOD\api\core\include ..\src\*.cpp ..\external\pugixml\lib\pugixml.lib ..\external\FMOD\api\core\lib\x64\fmod_vc.lib shell32.lib ..\external\SDL2\lib\x64\SDL2.lib ..\external\SDL2\lib\x64\SDL2main.lib ..\external\SDL2_image\lib\x64\SDL2_image.lib ..\external\FMOD\api\studio\lib\x64\fmodstudio_vc.lib /link /DEBUG:FASTLINK /SUBSYSTEM:CONSOLE
+cl /nologo /Fe:Game.exe /DDEBUG /EHsc /MP /Zi /I ..\external\box2d\include /I ..\external\pugixml\include /I ..\external\SDL2\include /I ..\external\SDL2_image\include /I ..\external\FMOD\api\studio\include /I ..\external\FMOD\api\core\include ..\src\*.cpp ..\external\box2d\lib\x64\box2d.lib ..\external\pugixml\lib\pugixml.lib ..\external\FMOD\api\core\lib\x64\fmod_vc.lib shell32.lib ..\external\SDL2\lib\x64\SDL2.lib ..\external\SDL2\lib\x64\SDL2main.lib ..\external\SDL2_image\lib\x64\SDL2_image.lib ..\external\FMOD\api\studio\lib\x64\fmodstudio_vc.lib /link /DEBUG:FASTLINK /SUBSYSTEM:CONSOLE
 popd
 
 echo.
