b9739c3e04adbaf38a831fc42cc92af8a4e8613b
Vitaly <vitalijskrumins@gmail.com>
2021-06-23 22:12:31 +0200

    Draw any polygon shapes

--- src/Box2DDebugRenderer.cpp
+++ src/Box2DDebugRenderer.cpp
@@ -6,14 +6,13 @@ Box2DDebugRenderer::Box2DDebugRenderer() : b2Draw() {
 };
 
 void Box2DDebugRenderer::DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) {
-    for (int32 i = 0; i < vertexCount; i += 4) {
-	b2Vec2 v1 = vertices[i];
-	b2Vec2 v2 = vertices[i + 1];
-	b2Vec2 v3 = vertices[i + 2];
-	b2Vec2 v4 = vertices[i + 3];
+    for (int32 i = 0; i < vertexCount - 1; i++) {
+	const b2Vec2 v1 = vertices[i];
+	const b2Vec2 v2 = vertices[i + 1];
 	DebugRenderer::DrawLine(v1.x, v1.y, v2.x, v2.y);
-	DebugRenderer::DrawLine(v2.x, v2.y, v3.x, v3.y);
-	DebugRenderer::DrawLine(v3.x, v3.y, v4.x, v4.y);
-	DebugRenderer::DrawLine(v4.x, v4.y, v1.x, v1.y);
     }
+
+    const b2Vec2 first = vertices[0];
+    const b2Vec2 last = vertices[vertexCount - 1];
+    DebugRenderer::DrawLine(first.x, first.y, last.x, last.y);
 }
