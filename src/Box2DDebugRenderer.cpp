2f98c91476b28280c77c53f6dc47feb272100df0
Vitaly <vitalijskrumins@gmail.com>
2021-07-13 12:20:52 +0200

    Draw proper body shapes instead of the fat AABBs

--- src/Box2DDebugRenderer.cpp
+++ src/Box2DDebugRenderer.cpp
@@ -2,12 +2,10 @@
 #include "DebugRenderer.h"
 
 Box2DDebugRenderer::Box2DDebugRenderer() : b2Draw() {
-    SetFlags(e_aabbBit | e_shapeBit | e_centerOfMassBit);
+    SetFlags(e_shapeBit);
 };
 
-void Box2DDebugRenderer::DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) {
-    // BUG: For some reason the vertices now have some significant fractional component added to them,
-    // which inflates the bounding box in all directions.
+void Box2DDebugRenderer::DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) {
     for (int32 i = 0; i < vertexCount - 1; i++) {
 	const b2Vec2 v1 = vertices[i];
 	const b2Vec2 v2 = vertices[i + 1];
--- src/Box2DDebugRenderer.h
+++ src/Box2DDebugRenderer.h
@@ -6,8 +6,8 @@
 class Box2DDebugRenderer : public b2Draw {
 public:
     Box2DDebugRenderer();
-    void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) override;
-    void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) override {};
+    void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) override {};
+    void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) override;
     void DrawCircle(const b2Vec2& center, float radius, const b2Color& color) override {};
     void DrawSolidCircle(const b2Vec2& center, float radius, const b2Vec2& axis, const b2Color& color) override {};
     void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color) override {};
