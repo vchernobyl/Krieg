e5c2798d541e3adcd15bf1e2fc7593630e74aad1
Vitaly <vitalijskrumins@gmail.com>
2021-08-10 19:36:06 +0200

    Add empty implementation of the vertex array

---
+++ src/VertexArray.cpp
@@ -0,0 +1,8 @@
+#include "VertexArray.h"
+
+VertexArray::VertexArray(const float* vertices, unsigned int vertexCount,
+			 const unsigned int* indices, unsigned int indexCount) {
+}
+
+VertexArray::~VertexArray() {
+}
---
+++ src/VertexArray.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class VertexArray {
+public:
+    VertexArray(const float* vertices, unsigned int vertexCount,
+		const unsigned int* indices, unsigned int indexCount);
+    ~VertexArray();
+
+    void SetActive();
+    unsigned int GetIndexCount() const { return indexCount; }
+    unsigned int GetVertexCount() const { return vertexCount; }
+
+private:
+    unsigned int vertexCount;
+    unsigned int indexCount;
+
+    unsigned int vertexBuffer; // OpenGL id for vertex buffer.
+    unsigned int indexBuffer;  // OpenGL id for index buffer.
+    unsigned int vertexArray;  // OpenGL id for vertex array object.
+};
