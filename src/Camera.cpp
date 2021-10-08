fd46950e576810e180fef4bcd5db109dc6bc7c07
Vitaly <vitalijskrumins@gmail.com>
2021-10-08 10:34:52 +0200

    Empty camera class

---
+++ src/Camera.cpp
@@ -0,0 +1,5 @@
+#include "Camera.h"
+
+Camera::Camera(int screenWidth, int screenHeight)
+    : screenWidth(screenWidth), screenHeight(screenHeight) {
+}
---
+++ src/Camera.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Camera {
+public:
+    Camera(int screenWidth, int screenHeight);
+private:
+    int screenWidth = 0;
+    int screenHeight = 0;
+};
